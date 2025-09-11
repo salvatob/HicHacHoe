94dcf028986af0dcc54632c82265d2e731b90ab6
Tobiáš Salva <tobias@avlas.cz>
2025-09-11 15:38:41 +0200

    initial

---
+++ app/Main.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import  Board.Printing
+
+main :: IO ()
+main = do
+  putStrLn "Hello, Haskell!"
+  -- MyLib.someFunc
+  Board.Printing.someFunc
---
+++ src/Board/Printing.hs
@@ -0,0 +1,4 @@
+module Board.Printing where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
