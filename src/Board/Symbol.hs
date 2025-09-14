807f0a6c5b631456e0dd0e84ba4580dce68d6f47
Tobiáš Salva <tobias@avlas.cz>
2025-09-14 19:41:24 +0200

    Add default Eq implementation to Symbol

--- src/Board/Symbol.hs
+++ src/Board/Symbol.hs
@@ -1,9 +1,11 @@
 module Board.Symbol where
 
 data Symbol = O | X | E
-  deriving (Show)
+  deriving (Show, Eq)
 
 renderSymbol :: Symbol -> Char
 renderSymbol O = 'O'
 renderSymbol X = 'X'
 renderSymbol E = '.'
+
+-- instance Show [Symbol] where
