9a86a6e6584d5f8cbcb6d1285b93361ba02beafd
Tobiáš Salva <tobias@avlas.cz>
2025-09-14 19:42:14 +0200

    Add several methods to the Board class

--- src/Board/Board.hs
+++ src/Board/Board.hs
@@ -1,10 +1,18 @@
 module Board.Board where
 
-import Board.Symbol ( Symbol )
+import Board.Symbol 
 
 
-class  Board b where
+class Board b where
   printBoard ::  b -> IO ()
   empty :: Int -> Int -> b
   placeS :: (Int, Int) -> b -> Symbol -> b
   getS ::  (Int, Int) -> b -> Symbol
+  getRow :: Int -> b -> [Symbol]
+  getCol :: Int -> b -> [Symbol]
+  getWidth :: b -> Int
+  getHeight :: b -> Int
+  allRows :: b -> [[Symbol]]
+  allCols :: b -> [[Symbol]]
+  allDiagonals :: b -> [[Symbol]]
+
