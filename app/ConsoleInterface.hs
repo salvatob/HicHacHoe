2eb5a3a4f032e4a340fcc9b204e751090f591c2d
Tobiáš Salva <tobias@avlas.cz>
2025-09-18 12:32:22 +0200

    Implement some IO functions

--- app/ConsoleInterface.hs
+++ app/ConsoleInterface.hs
@@ -24,6 +24,64 @@ readNextMove = do
   return $ parseLine line
 
 
--- read :: IO (Int, Int)
--- read = do
---   putStrLn "Please "
+queryFirstPlayer :: IO Bool
+queryFirstPlayer = do
+  putStrLn "Do you want to play first? (y/n)"
+
+  answer <- getLine
+
+  if elem 'y' answer
+    then return True
+  else
+    if elem 'n' answer
+      then return False
+    else error "You have not answered correctly (y/n)"
+
+
+
+data Opponent = Human | Computer
+  -- deriving EQ
+
+queryOpponent :: IO Opponent
+queryOpponent = do
+  putStrLn "Who do you want to play against? ('1'/'2')"
+  putStrLn "1. another human player"
+  putStrLn "2. AI player"
+
+  answer <- getLine
+
+  if elem '1' answer
+    then return Human
+  else
+    if elem '2' answer
+      then return Computer
+    else error "You have not answered correctly (1/2)"
+
+data GameMode = TTT | ConnectX
+  -- deriving (EQ)
+
+
+queryGameMode :: IO GameMode
+queryGameMode = do
+  putStrLn "Which game do you want to play? ('1'/'2')"
+  putStrLn "1. standard TicTacToe"
+  putStrLn "2. larger version - connect X"
+
+  answer <- getLine
+
+  if elem '1' answer
+    then return TTT
+  else
+    if elem '2' answer
+      then return ConnectX
+    else error "You have not answered correctly (1/2)"
+
+-- queryGameSize :: IO Int
+-- queryGameSize = do
+--   putStrLn "How many symbols to connect?"
+--   answer <- getLine
+
+--   let num = takeWhile isDigit $ dropWhile (not . isDigit) answer
+  
+--   let res = read num :: Int
+--   return res
--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,59 @@
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Redundant return" #-}
+-- {-# HLINT ignore "Redundant return" #-}
 module Main where
 
 import Board.Board
 import Board.MyBoardInstance
+import Board.BigBoard
 import AI.Evaluator
 import AI.Minimax
 import ConsoleInterface
 import Board.Symbol
 
-
--- toList :: MyBoard -> [[Symbol]]
--- toList (MyBoard b) = b
-
 main :: IO ()
 main = do
 
-  let b0 = empty :: MyBoard
+  let b0 = empty :: BigBoard
+  -- let b0 = empty :: MyBoard
+  printBoard b0
+
+  -- let b1 = placeS ((2,2)) b0 X
+  -- printBoard b1
+  
+  -- let b2 = placeS ((1,4)) b1 O
+  -- printBoard b2
+
   result <- play b0 False
   putStrLn $ renderSymbol result ++ " has won"
 
   return ()
 
 
+
+askGamemode :: (Board b) => IO (b, Int)
+askGamemode = do
+  gamemode <- queryGameMode :: IO GameMode
+
+  case gamemode of
+    TTT -> do
+      let ttt = empty :: MyBoard
+      return (ttt, 3)
+
+    ConnectX -> do
+      winLength <- queryGameSize :: IO Int
+      let mode = empty :: BigBoard
+      return (mode, winLength)
+
+  error "Invalid game mode has been selected"
+  -- return ()
+
+
 play :: (Board b) => b -> Bool -> IO Symbol
-play b _ | isTerminal 3 b = do return $ gameFinish 3 b
+play b _ | isTerminal winLength b = do return $ gameFinish winLength b
+  where
+    winLength = 4
 
 play b False = do
-  let nextMove = computerMove X b -- :: Board
+  let nextMove = computerMove O b -- :: Board
   printBoard nextMove
   result <-  play nextMove True
   return result
